b2e347376c4ad68d78b969118589d4fe63ff6ea3
xAKLx <xaklx@hotmail.com>
2016-04-05 14:24:34 -0400

    Creation of Filter abstract class

---
+++ PipesFilterExample/PipesFilter/AFilter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace PipesFilter
+{
+    /// <summary>
+    /// Filter abstract class for implementation.
+    /// </summary>
+    abstract class AFilter
+    {
+        /// <summary>
+        /// Defines if the Filter is running.
+        /// </summary>
+        public bool Stopped { get; set; }
+
+        /// <summary>
+        /// Set Stpped ot false.
+        /// </summary>
+        public AFilter()
+        {
+            Stopped = false;
+        }
+
+        /// <summary>
+        /// Run the process method until Stopped is true.
+        /// </summary>
+        public void Run()
+        {
+            while(!Stopped)
+            {
+                process();
+            }
+        }
+
+        /// <summary>
+        /// Process data.
+        /// </summary>
+        protected abstract void process();
+    }
+}
