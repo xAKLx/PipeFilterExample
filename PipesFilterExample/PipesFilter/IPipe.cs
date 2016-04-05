4b44f36154fe61146b61031f36a41d556a5a2e1d
xAKLx <xaklx@hotmail.com>
2016-04-05 12:46:31 -0400

    Documenting IPipe
    #1

--- PipesFilterExample/PipesFilter/IPipe.cs
+++ PipesFilterExample/PipesFilter/IPipe.cs
@@ -4,11 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//Class library that defines the components of a Pipes and Filter pattern
 namespace PipesFilter
 {
+    /// <summary>
+    /// Representative interface of a pipe.
+    /// </summary>
+    /// <typeparam name="T">Type of data.</typeparam>
     interface IPipe<T>
     {
+        /// <summary>
+        /// Adds data to the pipe if possible.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns>If possible true, else return false </returns>
         bool AddData(T data);
+
+        /// <summary>
+        /// Returns the oldest data.
+        /// </summary>
+        /// <returns>Data or null if not data</returns>
         T getData();
+        
     }
 }
