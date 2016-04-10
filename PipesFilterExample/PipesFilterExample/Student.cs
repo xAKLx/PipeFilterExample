ae5b4aa7b6731dc5fd4809de9a54a3a78ee738fb
xAKLx <xaklx@hotmail.com>
2016-04-09 22:29:14 -0400

    Create Student class

---
+++ PipesFilterExample/PipesFilterExample/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PipesFilterExample
+{
+    public class Student
+    {
+        public String Name { get; set; }
+        public uint Matricula { get; set; }
+        public uint Age { get; set; }
+        public bool IsMale { get; set; }
+
+        public Student(String name, uint matricula, uint age, bool isMale)
+        {
+            Name = name;
+            Matricula = matricula;
+            Age = age;
+            IsMale = isMale;
+        }
+    }
+}
