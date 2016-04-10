2041b2ebec50aaa61bbc1b10a8860fe31b5d1744
xAKLx <xaklx@hotmail.com>
2016-04-09 22:58:28 -0400

    Create Garbage pipe

---
+++ PipesFilterExample/PipesFilter/GarbagePipe.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PipesFilter
+{
+    /// <summary>
+    /// Simple Pipe, Do not store data.
+    /// </summary>
+    /// <typeparam name="T">Data type</typeparam>
+    public class GarbagePipe<T>: Pipe<T>
+    {
+        public GarbagePipe(): base(1)
+        {
+
+        }
+
+        public new bool AddData(T data)
+        {
+            return true;
+        }
+
+    }
+}
